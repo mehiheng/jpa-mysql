fc6fd191050e831c59b59e160b527e4b702296fa
ZHOUAD <Jing690518>
2018-07-27 13:51:23 +0800

    jpa-docker

---
+++ src/main/java/com/example/db/connectmysql/ConnectMysqlApplication.java
@@ -0,0 +1,12 @@
+package com.example.db.connectmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectMysqlApplication.class, args);
+	}
+}
